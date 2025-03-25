c80c11ae7f18b4476304c6f7bef0973d41b17368
Sudheesh T D <sudheeshtd01@gmail.com>
2025-03-25 02:28:38 -0500

    Added Send Message Controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -31,3 +31,32 @@ export const getMessages = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //realtime Func Goes Here(Socket.io)
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error in SendMessage Controller:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
