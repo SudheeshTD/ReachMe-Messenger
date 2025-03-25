52408c7d2a704897b21e8ea36f47c0e3588d1740
Sudheesh T D <sudheeshtd01@gmail.com>
2025-03-25 02:17:36 -0500

    Added GetMessage Controller and route

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,33 @@
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessages Controller:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -13,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server Is Running on port:" + PORT);
