0f128394361ce928fe31f8c35b958f6ccb2662ab
Sudheesh T D <sudheeshtd01@gmail.com>
2025-03-26 19:52:38 -0500

    Signup page Completed - SOme Lucid Icons not working

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
@@ -17,6 +18,17 @@ export const useAuthStore = create((set) => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ usSignUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account Created Successfully");
+    } finally {
+      set({ isSigningUp: false });
     }
   },
 }));
