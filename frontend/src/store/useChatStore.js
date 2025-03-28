6a7477dba6c7cc9e228e52930c4cdf9206c0dfd7
Sudheesh T D <sudheeshtd01@gmail.com>
2025-03-28 18:19:17 -0500

    Added Homepage-Sidep=bar- users visible

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -23,7 +24,7 @@ export const useChatStore = create((set) => ({
   getMessages: async (userId) => {
     set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`messages/${userId}`);
+      const res = await axiosInstance.get(`/messages/${userId}`);
       set({ messages: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
